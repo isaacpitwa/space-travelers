7df9cbc576fd9563412cb1824c98ca03abb044f2
isaacpitwa <isaacpitwa256@gmail.com>
2022-05-10 21:36:32 +0300

    set up rockets reducer

--- src/Redux/Rockets/rockets.js
+++ src/Redux/Rockets/rockets.js
@@ -1,57 +1,24 @@
-import ApiClient from '../../service/Apiclient';
+const FETCHED_ROCKETS = 'spaceTravelers/rockets/FETCHED_ROCKETS';
+import ApiClient from '../../service/Apiclient'
 
-const FETCHED_BOOKS = 'bookstore/books/FETCHED_BOOKS';
-const ADD_NEW_BOOK = 'bookstore/books/ADD_BOOK';
-const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-
-export function addNewBook(newBook) {
+export function fetchedRockets(rockets) {
+  const formattedRockets = rockets.map((rocket)=>{return {id: rocket.id,name:rocket.name, type: rocket.type,flickr_images:rocket.flickr_images }});
   return {
-    type: ADD_NEW_BOOK,
-    newBook,
+    type: FETCHED_ROCKETS,
+    rockets: formattedRockets,
   };
 }
 
-export function removeBook(book) {
-  return {
-    type: REMOVE_BOOK,
-    book,
-  };
-}
-
-export function fetchedBooks(books) {
-  const formattedBooks = Object.entries(books).map(([key, value]) => ({ ...value[0], id: key }));
-  return {
-    type: FETCHED_BOOKS,
-    books: formattedBooks,
-  };
-}
-
-export const fetchBooks = () => async (dispatch) => {
-  const response = await ApiClient.fetchAPIBooks();
-  dispatch(fetchedBooks(response));
+export const fetchRockets = () => async (dispatch) => {
+  const response = await ApiClient.fetchRockets();
+  dispatch(fetchedRockets(response));
 };
 
-export const AddNewBooks = (newBook) => async (dispatch) => {
-  await ApiClient.addBook(newBook);
-  dispatch(addNewBook({ ...newBook, id: newBook.item_id }));
-};
-
-export const removeBooks = (book) => async (dispatch) => {
-  await ApiClient.removeBook(book);
-  dispatch(removeBook({ ...book }));
-};
 
 export default function reducer(state = [], action = {}) {
   switch (action.type) {
-    case ADD_NEW_BOOK:
-      return [
-        ...state,
-        action.newBook,
-      ];
-    case REMOVE_BOOK:
-      return [...state.filter((book) => book.id !== action.book.id)];
-    case FETCHED_BOOKS:
-      return [...action.books];
+    case FETCHED_ROCKETS:
+      return [...action.rockets];
     default:
       return state;
   }
