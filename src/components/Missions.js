b3a26d894a2d2cc315e84cb0c4fd5ab615aedba1
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-05-10 11:39:58 -0500

    fetched data for missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMission } from '../Redux/Missions/missions';
 
 function Missions() {
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
+  useEffect(() => {
+    dispatch(getMission());
+  }, []);
+
   return (
-    <h2>Missions page</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      {missions.map((mission) => (
+        <tbody key={mission.mission_id}>
+          <tr>
+            <td>{mission.mission_name}</td>
+            <td>{mission.description}</td>
+            <td><button type="button">not a member</button></td>
+            <td><button type="button">join mission</button></td>
+          </tr>
+        </tbody>
+      ))}
+
+    </table>
   );
 }
 
