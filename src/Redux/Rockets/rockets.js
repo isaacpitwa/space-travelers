ffed83d5039c14f871ab845e65252d75134be744
isaacpitwa <isaacpitwa256@gmail.com>
2022-05-10 23:08:50 +0300

    define reserve rocket action

--- src/Redux/Rockets/rockets.js
+++ src/Redux/Rockets/rockets.js
@@ -1,7 +1,8 @@
 import ApiClient from '../../service/Apiclient';
 
-const FETCHED_ROCKETS = 'spaceTravelers/rockets/FETCHED_ROCKETS';
 const FETCHING_ROCKETS = 'spaceTravelers/rockets/FETCHING_ROCKETS';
+const FETCHED_ROCKETS = 'spaceTravelers/rockets/FETCHED_ROCKETS';
+const RESERVE_ROCKET = 'spaceTravelers/rockets/RESERVE_ROCKET';
 
 export function fetchedRockets(rockets) {
   const formattedRockets = rockets.map((rocket) => ({
@@ -29,6 +30,10 @@ export const fetchRockets = () => async (dispatch) => {
     dispatch(fetchedRockets(response));
   }, 1000);
 };
+export const reserveRocket = (rocket) => ({
+  type: RESERVE_ROCKET,
+  rocket,
+});
 
 export default function reducer(state = [], action = {}) {
   switch (action.type) {
@@ -36,6 +41,10 @@ export default function reducer(state = [], action = {}) {
       return 'fetching Rockets...';
     case FETCHED_ROCKETS:
       return [...action.rockets];
+    case RESERVE_ROCKET:
+      return state.map(
+        (rocket) => (rocket.id !== action.rocket.id ? rocket : { ...rocket, reserved: true }),
+      );
     default:
       return state;
   }
