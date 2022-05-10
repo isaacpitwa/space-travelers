400ea522f956e70236d73c153e99d52c6936fce0
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-05-10 12:27:51 -0500

    table of missions styled

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 import { getMission } from '../Redux/Missions/missions';
 
 function Missions() {
@@ -10,7 +12,7 @@ function Missions() {
   }, []);
 
   return (
-    <table>
+    <Table striped bordered hover size="sm">
       <thead>
         <tr>
           <th>Mission</th>
@@ -18,18 +20,23 @@ function Missions() {
           <th>Status</th>
         </tr>
       </thead>
-      {missions.map((mission) => (
-        <tbody key={mission.mission_id}>
-          <tr>
+      <tbody>
+        {missions.map((mission) => (
+          <tr key={mission.mission_id}>
             <td>{mission.mission_name}</td>
             <td>{mission.description}</td>
-            <td><button type="button">not a member</button></td>
-            <td><button type="button">join mission</button></td>
+            <td className="member-btn">
+              <Button variant="secondary" as="input" type="button" value="not a member" />
+              {' '}
+            </td>
+            <td className="member-btn">
+              <Button variant="outline-secondary" as="input" type="button" value="Join Mission" />
+              {' '}
+            </td>
           </tr>
-        </tbody>
-      ))}
-
-    </table>
+        ))}
+      </tbody>
+    </Table>
   );
 }
 
